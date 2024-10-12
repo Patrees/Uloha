c1ea6b3f03f9270aa69e6e1a44791948cc42cef8
Patrees <patolang@gmail.com>
2024-10-12 10:26:24 +0200

    uprava function.js kvoli nefunkcnemu tlacidlu na pridanie novej card

--- js/function.js
+++ js/function.js
@@ -1,78 +1,32 @@
-import { fetchCardsDatabase, insertCardsDatabase, deleteCardsDatabase } from "./home.js";
+import {
+  fetchCardsDatabase,
+  insertCardsDatabase,
+  deleteCardsDatabase,
+} from "./home.js";
 
+window.addEventListener("load", function () {
+  const list = document.querySelector(".cards ul");
+  const button = document.querySelector(".btn");
+  const form = document.querySelector("form");
 
+  let draggedItem = null;
 
-        // Funkcia pridanie karty po stlaceni tlacidla //
+  //Funkcia na vytvorenie HTML card z objektu pola //
+  
+  function createCardElement(card) {
+    const newElement = document.createElement("li");
+    newElement.classList.add("sdk");
+    newElement.setAttribute("draggable", "true");
 
-    async function submitButton(event) {
-    event.preventDefault();
+    const newCloseIcon = document.createElement("img");
+    newCloseIcon.src = "./images/close-red-icon.svg";
+    newCloseIcon.classList.add("close-icon");
+    newCloseIcon.setAttribute("data-id", card.id);
 
-    let headingValue = document.querySelector('input[name="HeaderCard"]').value.trim();
-    let paragraphValue = document.querySelector('textarea[name="TextCard"]').value.trim();
-    const existingErrorMessage = document.querySelector('.error-message');
-    
-
-    if (headingValue === '' || paragraphValue === '') {
-        if (!existingErrorMessage) {
-            const errorMessage = document.createElement('p');
-            errorMessage.classList.add('animate__animated', 'animate__bounce', 'error-message');
-            errorMessage.textContent = 'Vyplň prosim ta formulár';
-            form.insertBefore(errorMessage, form.firstChild);
-        }
-    }else {
-        if (existingErrorMessage) {
-            existingErrorMessage.classList.add('animate__animated', 'animate__backOutDown')
-            setTimeout( function() {
-                form.removeChild(existingErrorMessage); 
-            }, 500);
-        }
-        insertCardsDatabase (headingValue, paragraphValue);
-        createCardElement({heading: headingValue, paragraph: paragraphValue});
-        form.reset();
-    }
-}
-
-
-        // Funkcia odstránenie karty z databazy //
-
-        async function deleteFromList(event) {
-            if (event.target.classList.contains('close-icon')) {
-                event.target.parentElement.classList.add('animate__animated', 'animate__backOutDown');
-                const cardID = event.target.getAttribute('data-id');
-                const isdeleted = deleteCardsDatabase(cardID)
-                if (isdeleted) {
-
-                    setTimeout( function() {
-                        event.target.parentElement.remove(); 
-                    }, 500);
-
-                }
-        }   
-    }
-
-window.addEventListener('load', function() {
-    
-    const list = document.querySelector('.cards ul');
-    const button = document.querySelector('.btn');
-    const form = document.querySelector('form');
-
-    let draggedItem = null;
-
- //Funkcia na vytvorenie HTML card z objektu pola //
- function createCardElement(card) {
-    const newElement = document.createElement('li');
-    newElement.classList.add('sdk');
-    newElement.setAttribute('draggable', 'true');
-
-    const newCloseIcon = document.createElement('img');
-    newCloseIcon.src = './images/close-red-icon.svg';
-    newCloseIcon.classList.add('close-icon');
-    newCloseIcon.setAttribute('data-id', card.id);
-    
-    const newHeading = document.createElement('h3');
+    const newHeading = document.createElement("h3");
     newHeading.textContent = card.heading;
 
-    const newParagraph = document.createElement('p');
+    const newParagraph = document.createElement("p");
     newParagraph.textContent = card.paragraph;
 
     newElement.appendChild(newCloseIcon);
@@ -80,63 +34,109 @@ window.addEventListener('load', function() {
     newElement.appendChild(newParagraph);
 
     list.appendChild(newElement);
-}
+  }
 
+  //funkcia na zobrazenie card//
 
-    //funkcia na zobrazenie card//
-    
-    async function displayAllCards() {
-        const cardsArray = await fetchCardsDatabase();
+  async function displayAllCards() {
+    const cardsArray = await fetchCardsDatabase();
 
-        cardsArray.forEach(function(card) {
-            createCardElement(card);  
-        });
+    cardsArray.forEach(function (card) {
+      createCardElement(card);
+    });
+  }
+
+  button.addEventListener("click", submitButton);
+  list.addEventListener("click", deleteFromList);
+
+  displayAllCards();
+
+  // Drag and drop funkcionalita //
+
+  list.addEventListener("dragstart", function (event) {
+    if (event.target.tagName === "LI") {
+      draggedItem = event.target;
+      setTimeout(function () {
+        draggedItem.classList.add("dragging");
+      }, 0);
     }
+  });
 
-    button.addEventListener('click', submitButton);
-    list.addEventListener('click', deleteFromList);
-   
-    displayAllCards();
+  list.addEventListener("dragend", function (event) {
+    if (event.target.tagName === "LI") {
+      setTimeout(function () {
+        draggedItem.classList.remove("dragging");
+        draggedItem = null;
+      }, 0);
+    }
+  });
 
-    // Drag and drop funkcionalita //
+  list.addEventListener("dragover", function (event) {
+    event.preventDefault();
+  });
 
-    list.addEventListener('dragstart', function(event) {
-        if (event.target.tagName === 'LI') {
-            draggedItem = event.target;
-            setTimeout(function() {
-                draggedItem.classList.add('dragging');
-            }, 0);
-        }
-    });
+  list.addEventListener("drop", function (event) {
+    event.preventDefault();
+    if (event.target.tagName === "LI" && draggedItem !== event.target) {
+      list.insertBefore(draggedItem, event.target);
+    }
+  });
 
-    list.addEventListener('dragend', function(event) {
-        if (event.target.tagName === 'LI') {
-            setTimeout(function() {
-                draggedItem.classList.remove('dragging')
-                draggedItem = null;
-            }, 0);
-        }
-    });
+  // Funkcia pridanie karty po stlaceni tlacidla //
 
-    list.addEventListener('dragover', function(event) {
-        event.preventDefault();
-    });
+  function submitButton(event) {
+    event.preventDefault();
 
-    list.addEventListener('drop', function(event) {
-        event.preventDefault();
-        if (event.target.tagName === 'LI' && draggedItem !== event.target) {
-            list.insertBefore(draggedItem, event.target);
-        }
-            
-    });
+    let headingValue = document
+      .querySelector('input[name="HeaderCard"]')
+      .value.trim();
+    let paragraphValue = document
+      .querySelector('textarea[name="TextCard"]')
+      .value.trim();
+    const existingErrorMessage = document.querySelector(".error-message");
 
+    if (headingValue === "" || paragraphValue === "") {
+      if (!existingErrorMessage) {
+        const errorMessage = document.createElement("p");
+        errorMessage.classList.add(
+          "animate__animated",
+          "animate__bounce",
+          "error-message"
+        );
+        errorMessage.textContent = "Vyplň prosim ta formulár";
+        form.insertBefore(errorMessage, form.firstChild);
+      }
+    } else {
+      if (existingErrorMessage) {
+        existingErrorMessage.classList.add(
+          "animate__animated",
+          "animate__backOutDown"
+        );
+        setTimeout(function () {
+          form.removeChild(existingErrorMessage);
+        }, 500);
+      }
+      insertCardsDatabase(headingValue, paragraphValue);
+      createCardElement({ heading: headingValue, paragraph: paragraphValue });
+      form.reset();
+    }
+  }
 
+  // Funkcia odstránenie karty z databazy //
+
+  async function deleteFromList(event) {
+    if (event.target.classList.contains("close-icon")) {
+      event.target.parentElement.classList.add(
+        "animate__animated",
+        "animate__backOutDown"
+      );
+      const cardID = event.target.getAttribute("data-id");
+      const isdeleted = deleteCardsDatabase(cardID);
+      if (isdeleted) {
+        setTimeout(function () {
+          event.target.parentElement.remove();
+        }, 500);
+      }
+    }
+  }
 });
-
-
-  
-       
-
-
-
-
