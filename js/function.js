05e32f3e22fc61b6f2d46ebe4f1ffb5de0c8c940
Patrees <patolang@gmail.com>
2024-10-13 11:31:10 +0200

    uprava funkcie na editaciu

--- js/function.js
+++ js/function.js
@@ -51,6 +51,8 @@ window.addEventListener("load", function () {
     cardsArray.forEach(function (card) {
       createCardElement(card);
     });
+
+    displayAllCards();
   }
 
   button.addEventListener("click", submitButton);
@@ -59,60 +61,60 @@ window.addEventListener("load", function () {
   // Funkcia na editaciu kariet//
 
 
-  function enableEdit(event) {
-    const element = event.target;
-    const originalText = element.textContent;
-    const type = element.tagName.toLowerCase();
+  // function enableEdit(event) {
+  //   const element = event.target;
+  //   const originalText = element.textContent;
+  //   const type = element.tagName.toLowerCase();
 
-    let input;
-    if (type === "h3") {
+  //   let input;
+  //   if (type === "h3") {
 
-      input = document.createElement("input");
+  //     input = document.createElement("input");
 
-    } else if (type === "p") {
+  //   } else if (type === "p") {
 
-      input = document.createElement("textarea");
+  //     input = document.createElement("textarea");
 
-    } 
+  //   } 
 
-    input.value = originalText;
-    input.classList.add("editing-input");
+  //   input.value = originalText;
+  //   input.classList.add("editing-input");
 
-    element.replaceWith(input);
-    input.focus();
+  //   element.replaceWith(input);
+  //   input.focus();
 
-    input.addEventListener("blur", function () {
-      const updatedText = input.value.trim();
+  //   input.addEventListener("blur", function () {
+  //     const updatedText = input.value.trim();
 
-      if (updatedText && updatedText !== originalText) {
-        updateCardContent(element, getAttribute("data-id"), type, updatedText);
-        input.replaceWith(element);
-      } else {
-        input.replaceWith(element);
-      }
-    });
+  //     if (updatedText && updatedText !== originalText) {
+  //       updateCardContent(element, getAttribute("data-id"), type, updatedText);
+  //       input.replaceWith(element);
+  //     } else {
+  //       input.replaceWith(element);
+  //     }
+  //   });
 
-  input.addEventListener("keydown", function (event) {
-    if (event.key === "Enter" && type === "h3") {
-      input.blur();
-    }
-  });
+  // input.addEventListener("keydown", function (event) {
+  //   if (event.key === "Enter" && type === "h3") {
+  //     input.blur();
+  //   }
+  // });
 
-  async function updateCardContent(id, type, updatedText) {
-    let updateField = {}
-    if (type === "h3") {
-      updateField = { heading: updatedText };
-    } else if (type === "p") {
-      updateField = { paragraph: updatedText };
-    }
+  // async function updateCardContent(id, type, updatedText) {
+  //   let updateField = {}
+  //   if (type === "h3") {
+  //     updateField = { heading: updatedText };
+  //   } else if (type === "p") {
+  //     updateField = { paragraph: updatedText };
+  //   }
 
-    const succes = await updateCardsDatabase(id, updateField);
-    if (succes) {
-      console.log("Uspesny update");
-    } else {
-      console.log("Nesuspesny update");
-    }
-  }
+  //   const succes = await updateCardsDatabase(id, updateField);
+  //   if (succes) {
+  //     console.log("Uspesny update");
+  //   } else {
+  //     console.log("Nesuspesny update");
+  //   }
+  // }
 
 
 
@@ -210,5 +212,5 @@ window.addEventListener("load", function () {
     }
   }
 
-  displayAllCards();
+
 });
