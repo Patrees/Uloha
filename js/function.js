f2dccb91ae27b93774cfa11af691fec3d6359b24
Patrees <patolang@gmail.com>
2024-10-13 15:53:20 +0200

    oprava funkcie update card

--- js/function.js
+++ js/function.js
@@ -50,109 +50,11 @@ window.addEventListener("load", function () {
   async function displayAllCards() {
     const cardsArray = await fetchCardsDatabase();
 
+    list.innerHTML = ""
     cardsArray.forEach(function (card) {
       createCardElement(card);
     });
 
-  }
-
-  button.addEventListener("click", submitButton);
-  list.addEventListener("click", deleteFromList);
-
- 
-
-  // Funkcia pridanie karty po stlaceni tlacidla //
-
-  function submitButton(event) {
-    event.preventDefault();
-
-    let headingValue = document
-      .querySelector('input[name="HeaderCard"]')
-      .value.trim();
-    let paragraphValue = document
-      .querySelector('textarea[name="TextCard"]')
-      .value.trim();
-    const existingErrorMessage = document.querySelector(".error-message");
-
-    if (headingValue === "" || paragraphValue === "") {
-      if (!existingErrorMessage) {
-        const errorMessage = document.createElement("p");
-        errorMessage.classList.add(
-          "animate__animated",
-          "animate__bounce",
-          "error-message"
-        );
-        errorMessage.textContent = "Vyplň prosim ta formulár";
-        form.insertBefore(errorMessage, form.firstChild);
-      }
-    } else {
-      if (existingErrorMessage) {
-        existingErrorMessage.classList.add(
-          "animate__animated",
-          "animate__backOutDown"
-        );
-        setTimeout(function () {
-          form.removeChild(existingErrorMessage);
-        }, 500);
-      }
-      insertCardsDatabase(headingValue, paragraphValue);
-      createCardElement({ heading: headingValue, paragraph: paragraphValue });
-      form.reset();
-    }
-  }
-
-
-
-  // Drag and drop funkcionalita //
-
-  list.addEventListener("dragstart", function (event) {
-    if (event.target.tagName === "LI") {
-      draggedItem = event.target;
-      setTimeout(function () {
-        draggedItem.classList.add("dragging");
-      }, 0);
-    }
-  });
-
-  list.addEventListener("dragend", function (event) {
-    if (event.target.tagName === "LI") {
-      setTimeout(function () {
-        draggedItem.classList.remove("dragging");
-        draggedItem = null;
-      }, 0);
-    }
-  });
-
-  list.addEventListener("dragover", function (event) {
-    event.preventDefault();
-  });
-
-  list.addEventListener("drop", function (event) {
-    event.preventDefault();
-    if (event.target.tagName === "LI" && draggedItem !== event.target) {
-      list.insertBefore(draggedItem, event.target);
-    }
-  });
-
-  
-
-  // Funkcia odstránenie karty z databazy //
-
-
-  function deleteFromList(event) {
-    if (event.target.classList.contains("close-icon")) {
-      event.target.parentElement.classList.add(
-        "animate__animated",
-        "animate__backOutDown"
-      );
-      const cardID = event.target.getAttribute("data-id");
-      const isdeleted = deleteCardsDatabase(cardID);
-      if (isdeleted) {
-        setTimeout(function () {
-          event.target.parentElement.remove();
-        }, 500);
-      }
-    }
   }
 
   // Funkcia editovanie cards //
@@ -213,6 +115,107 @@ window.addEventListener("load", function () {
     }
 
   }
+
+  button.addEventListener("click", submitButton);
+  list.addEventListener("click", deleteFromList);
+
   displayAllCards();
 
+  // Funkcia pridanie karty po stlaceni tlacidla //
+
+  function submitButton(event) {
+    event.preventDefault();
+
+    let headingValue = document
+      .querySelector('input[name="HeaderCard"]')
+      .value.trim();
+    let paragraphValue = document
+      .querySelector('textarea[name="TextCard"]')
+      .value.trim();
+    const existingErrorMessage = document.querySelector(".error-message");
+
+    if (headingValue === "" || paragraphValue === "") {
+      if (!existingErrorMessage) {
+        const errorMessage = document.createElement("p");
+        errorMessage.classList.add(
+          "animate__animated",
+          "animate__bounce",
+          "error-message"
+        );
+        errorMessage.textContent = "Vyplň prosim ta formulár";
+        form.insertBefore(errorMessage, form.firstChild);
+      }
+    } else {
+      if (existingErrorMessage) {
+        existingErrorMessage.classList.add(
+          "animate__animated",
+          "animate__backOutDown"
+        );
+        setTimeout(function () {
+          form.removeChild(existingErrorMessage);
+        }, 500);
+      }
+      insertCardsDatabase(headingValue, paragraphValue);
+      createCardElement({ heading: headingValue, paragraph: paragraphValue });
+      form.reset();
+    }
+    
+  }
+
+
+
+  // Drag and drop funkcionalita //
+
+  list.addEventListener("dragstart", function (event) {
+    if (event.target.tagName === "LI") {
+      draggedItem = event.target;
+      setTimeout(function () {
+        draggedItem.classList.add("dragging");
+      }, 0);
+    }
+  });
+
+  list.addEventListener("dragend", function (event) {
+    if (event.target.tagName === "LI") {
+      setTimeout(function () {
+        draggedItem.classList.remove("dragging");
+        draggedItem = null;
+      }, 0);
+    }
+  });
+
+  list.addEventListener("dragover", function (event) {
+    event.preventDefault();
+  });
+
+  list.addEventListener("drop", function (event) {
+    event.preventDefault();
+    if (event.target.tagName === "LI" && draggedItem !== event.target) {
+      list.insertBefore(draggedItem, event.target);
+    }
+  });
+
+  
+
+  // Funkcia odstránenie karty z databazy //
+
+
+  function deleteFromList(event) {
+    if (event.target.classList.contains("close-icon")) {
+      event.target.parentElement.classList.add(
+        "animate__animated",
+        "animate__backOutDown"
+      );
+      const cardID = event.target.getAttribute("data-id");
+      const isdeleted = deleteCardsDatabase(cardID);
+      if (isdeleted) {
+        setTimeout(function () {
+          event.target.parentElement.remove();
+        }, 500);
+      }
+    }
+  }
+
+  
+  displayAllCards();
 });
